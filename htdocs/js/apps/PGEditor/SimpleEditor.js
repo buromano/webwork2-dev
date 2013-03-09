55eb5d546cde42d500d74c907d666aec7b0482c5
Bruce Romano <bromano1@fitchburgstate.edu>
2013-03-09 13:21:45 -0500

    updateupdate

--- htdocs/js/apps/PGEditor/SimpleEditor.js
+++ htdocs/js/apps/PGEditor/SimpleEditor.js
@@ -42,7 +42,7 @@ function(Backbone, _,WebPage){
             this.render();
         },
         render: function (){
-            this.libraryTreeView = new LibraryTreeVuew({parent})
+          #  this.libraryTreeView = new LibraryTreeVuew({parent})
             
         },
         events: {"click #build-script": "buildScript"},
